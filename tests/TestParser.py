3c16b9634f4e45c12e480796be53b9291b1ce74a
oyevtushe <casio6290@gmail.com>
2019-12-11 23:12:04 +0200

    Add TestParser helper class for tests
    Signed-off-by: oyevtushe <casio6290@gmail.com>

---
+++ tests/TestParser.py
@@ -0,0 +1,91 @@
+import re
+
+class TestParserError(Exception):
+    pass
+
+class TestParser:
+    """This class gives more flexibility to tests.
+       It allows to change initial facts and 
+       questions, gives expected values for specified
+       facts. This is achieved via specific syntax
+       in comment lines
+
+       Example:
+
+       A => B
+       B | K => C + D
+
+       =K
+       ?D
+
+       #=AR +MT -SO
+
+       Notice:
+           Input should be valid for expert system
+       What does really important it's the last line
+       "#=AR +MT -SO" it changes initial facts to AR
+       and says that test should expect MT as True,
+       while SO as False, MT and SO also become
+       questions in parse() output:
+
+       A => B
+       B | K => C + D
+
+       =AR
+       ?MTSO
+
+       #=AR +MT -SO
+
+       also there'll be info about what facts should be false
+       and what true after processing such input by expert
+       system
+    """
+    def __init__(self, **kwargs):
+        """kwargs are checked for key 'file'
+           which specifies the path to the file
+           with valid expert system input and
+           for 'content' which specifies
+           multiline string with valid expert
+           system input
+        """
+        self._content = ''
+        if not 'file' in kwargs and not 'content' in kwargs:
+            raise TestParserError('file or content argument expected')
+        if 'content' in kwargs:
+            self._content = kwargs['content']
+        self._defines_format = r'(?m)^=[A-Z]*$'
+        self._questions_format = r'(?m)^\?[A-Z]+$'
+        self._cmnt_format = r'(?m)^#(?P<defines>=[A-Z]*)'
+        self._cmnt_format += r'( \+(?P<pos>[A-Z]+))?'
+        self._cmnt_format += r'( -(?P<neg>[A-Z]+))?$'
+        if not self._content:
+            with open(kwargs['file']) as f:
+                self._content = f.read()
+
+    def parse(self):
+        """parse() -> generator which
+           generates (text, pos, neg) for every
+           comment line in input with specified format as below.
+           'text' is multiline string
+           (modified input), 'pos' is list of
+           facts that should to be true and
+           'neg' which is a list of facts
+           that should to be false.
+
+           a simplified syntax is:
+           #=[A-Z]* \+[A-Z]+ -[A-Z]+
+
+           It's allowed to specify only - or + facts
+           but if both are absent TestParserError
+           is raised
+        """
+        for match in re.finditer(self._cmnt_format, self._content):
+            new_defines = match.group('defines')
+            pos = match.group('pos')
+            neg = match.group('neg')
+            if not pos and not neg:
+                raise TestParserError('+ or - facts must be defined')
+            new_questions = '?' + (pos or '') + (neg or '')
+            imds = re.sub(self._defines_format, new_defines, self._content) 
+            imds = re.sub(self._questions_format, new_questions, imds)
+            yield (imds, pos, neg)
