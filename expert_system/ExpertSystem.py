d7212975c13c85d704477f555cf3eb84cbeee263
oyevtushe <casio6290@gmail.com>
2019-12-15 15:29:02 +0200

    Change rule resolving algorithm
    Now it builds a graph with rules as vertices
    then traverses and resolves them with dfs
    
    Signed-off-by: oyevtushe <casio6290@gmail.com>

--- expert_system/ExpertSystem.py
+++ expert_system/ExpertSystem.py
@@ -5,7 +5,37 @@ from expert_system.ResolveLhs import ResolveLhs
 from expert_system.RhsValidator import RhsValidator
 from collections import defaultdict
 
-# if i met recursion, say it's recursion
+class _MyTree:
+    def __init__(self, rule):
+        self._rule = rule
+        self._clst = []
+
+    def add_child(self, child):
+        self._clst.append(child)
+
+    def find(self, rule):
+        if rule == self._rule:
+            return self
+        for child in self._clst:
+            return child.find(rule)
+        return None
+
+    def _visit_dfs(self, visited, fu):
+        visited.add(self._rule)
+        if self._rule:
+            logging.debug(f'Visited:{self._rule.pretty()}')
+        for child in self._clst:
+            if not child._rule in visited:
+                child._visit_dfs(visited, fu)
+            else:
+                logging.debug(f'Ignoring: {child._rule.pretty()}')
+        if self._rule:
+            fu(self._rule)
+
+    def visit_dfs(self, fu):
+        visited = set()
+        self._visit_dfs(visited, fu)
+
 class ExpertSystem:
     def __init__(self):
         logging.basicConfig()
@@ -34,20 +64,18 @@ class ExpertSystem:
         self._facts.clear()
         self._questions.clear()
 
+    def _rule_solver(self, rule):
+        res = self._rl.transform(rule.children[0], self._facts)
+        for rhv in list(rule.children[2].find_data('val')):
+            if not self._facts[rhv.children[0]]:
+                self._facts[rhv.children[0]] = res
+                logging.debug(f'set {rhv.children[0]} to {res}')
+
     def resolve(self, inp):
         self._parse_inp(inp)
-        stack = self._check_rules(self._questions)
-        if stack:
-            for rset in reversed(stack):
-                res = False
-                for r in rset:
-                    res = self._rl.transform(r.children[0], self._facts)
-                    if res:
-                        break
-                for rhv in list(r.children[2].find_data('val')):
-                    if not self._facts[rhv.children[0]]:
-                        self._facts[rhv.children[0]] = res
-                        logging.debug(f'set {rhv.children[0]} to {res}')
+        root = self._check_rules(self._questions)
+        root.visit_dfs(logging.debug)
+        root.visit_dfs(self._rule_solver)
         res = {q:self._facts[q] for q in self._questions}
         self._reset()
         return res
@@ -56,7 +84,7 @@ class ExpertSystem:
         res = set()
         for rule in list(self._tree.find_data('rule')):
             if any(x.children[0] == val for x in list(rule.children[2].find_data('val'))):
-                logging.debug(f'found {val} in {rule}')
+                logging.debug(f'found {val} in {rule.pretty()}')
                 res.add(rule)
         return res
         
@@ -74,33 +102,44 @@ class ExpertSystem:
             if not res:
                 return False
         return True
-            
 
-    def _check_rules(self, q):
-        lst = [] + q
-        stack = []
-        stacked_rules = set()
-        for var in lst:
-            rset = self._get_rules_with_rhs(var)
-            if not rset:
-                logging.debug(f'{var} there\'s no rule that resolves it, set it as False')
-                continue
-            if not self._check_rules_rhs(rset):
-                logging.debug(f'{var} is set as False, due to invalid rule for it')
-                continue
-            diff = rset.difference(stacked_rules)
-            if diff:
-                stack.append(diff)
-                stacked_rules.update(rset)
+    def _check_rules(self, qes):
+        root = _MyTree(None)
+        deplst = [_Dep(root, [q]) for q in qes]
+        for dep in deplst:
+            kiddo = dep.node
+            for var in dep.flst:
+                #TODO: replace with list
+                rset = self._get_rules_with_rhs(var)
+                if not rset:
+                    logging.debug(f'{var} there\'s no rule that resolves it, set it as False')
+                    continue
+                if not self._check_rules_rhs(rset):
+                    logging.debug(f'{var} is set as False, due to invalid rule for it')
+                    continue
                 for r in rset:
+                    logging.debug(f'Looking for {r.pretty()} in {root}')
+                    found = root.find(r)
+                    if found:
+                        logging.debug(f'rule {found._rule.pretty()} is already in the tree, linking')
+                        kiddo.add_child(found)
+                        continue
+                    new_kiddo = _MyTree(r)
+                    kiddo.add_child(new_kiddo)
+                    logging.debug(f'Adding new kiddo: {new_kiddo}')
+                    new_dep = (_Dep(new_kiddo, []))
                     for v in self._vars(r, 'l'):
                         if not v in self._facts:
                             logging.debug(f'{v} is not resolved, queue for resolving')
-                            lst.append(v)
+                            new_dep.flst.append(v)
                         else:
                             logging.debug(f'{v} is known')
-            else:
-                logging.debug(f'Stack already has a rule to resolve {var}')
-        logging.debug(f'stack: {stack}')
-        return stack
+                    if new_dep.flst:
+                        deplst.append(new_dep)
+        logging.debug(f'tree: {root.visit_dfs(print)}')
+        return root
 
+class _Dep:
+    def __init__(self, node, flst):
+        self.node = node
+        self.flst = flst
