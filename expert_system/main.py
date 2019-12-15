e0caf664f9fb0289500369ea6ce1eebfcefa0a95
oyevtushe <casio6290@gmail.com>
2019-12-15 18:14:39 +0200

    Catch and print exceptions
    Signed-off-by: oyevtushe <casio6290@gmail.com>

--- expert_system/main.py
+++ expert_system/main.py
@@ -7,15 +7,18 @@ from expert_system.ExpertSystem import ExpertSystem
 logging.basicConfig()
 
 def main(argc, argv):
-    with open(argv[1]) as f:
-        content = f.read()
-    logging.debug(f'content start (stripped)')
-    logging.debug(f'\n{content.strip()}')
-    logging.debug(f'content end')
-    es = ExpertSystem()
-    res = es.resolve(content)
-    for k,v in res.items():
-        print(f'{k} is {v}')
+    try:
+        with open(argv[1]) as f:
+            content = f.read()
+        logging.debug(f'content start (stripped)')
+        logging.debug(f'\n{content.strip()}')
+        logging.debug(f'content end')
+        es = ExpertSystem()
+        res = es.resolve(content)
+        for k,v in res.items():
+            print(f'{k} is {v}')
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
     main(len(sys.argv), sys.argv)
