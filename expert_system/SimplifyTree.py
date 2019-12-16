9d7647bd3443eceef81c0cc669da880fe9ea2362
oyevtushe <casio6290@gmail.com>
2019-12-17 00:55:51 +0200

    Make nested node case in SimplifyTree explicit
    Signed-off-by: oyevtushe <casio6290@gmail.com>

--- expert_system/SimplifyTree.py
+++ expert_system/SimplifyTree.py
@@ -5,27 +5,27 @@ class SimplifyTree(Transformer):
     """This class removes excessive nodes
     """
     def expr_and(self, child):
-        if len(child) < 2:
+        if len(child) == 1:
             return child[0]
         return Tree('expr_and', child)
 
     def expr_or(self, child):
-        if len(child) < 2:
+        if len(child) == 1:
             return child[0]
         return Tree('expr_or', child)
 
     def expr_xor(self, child):
-        if len(child) < 2:
+        if len(child) == 1:
             return child[0]
         return Tree('expr_xor', child)
 
     def expr_neg(self, child):
-        if len(child) < 2:
+        if len(child) == 1:
             return child[0]
         return Tree('expr_neg', child)
 
     def expr_eq(self, child):
-        if len(child) < 2:
+        if len(child) == 1:
             return child[0]
         return Tree('expr_eq', child)
 
