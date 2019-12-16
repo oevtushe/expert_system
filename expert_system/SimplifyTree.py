d1a5dd0078a46742f50dd4bffc29b4c0086b3fc7
oyevtushe <casio6290@gmail.com>
2019-12-17 01:00:25 +0200

    Improve doc in SimplifyTree
    Signed-off-by: oyevtushe <casio6290@gmail.com>

--- expert_system/SimplifyTree.py
+++ expert_system/SimplifyTree.py
@@ -5,29 +5,46 @@ class SimplifyTree(Transformer):
     """This class removes excessive nodes
     """
     def expr_and(self, child):
+        """Replaces expr_and node with
+           inner one if there's
+
+           Distinctive property is that
+           the current node has only one child
+        """
         if len(child) == 1:
             return child[0]
         return Tree('expr_and', child)
 
     def expr_or(self, child):
+        """Same as expr_and
+        """
         if len(child) == 1:
             return child[0]
         return Tree('expr_or', child)
 
     def expr_xor(self, child):
+        """Same as expr_and
+        """
         if len(child) == 1:
             return child[0]
         return Tree('expr_xor', child)
 
     def expr_neg(self, child):
+        """Same as expr_and
+        """
         if len(child) == 1:
             return child[0]
         return Tree('expr_neg', child)
 
     def expr_eq(self, child):
+        """Same as expr_and
+        """
         if len(child) == 1:
             return child[0]
         return Tree('expr_eq', child)
 
     def factor(self, child):
+        """Factor node is totaly
+           replaced by inner 'val'
+        """
         return child[0]
