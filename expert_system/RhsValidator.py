5dfca0f917163b35b09945b53dcebfaa3473068d
oyevtushe <casio6290@gmail.com>
2019-12-17 01:03:25 +0200

    Improve documentation for RhsValidator
    Signed-off-by: oyevtushe <casio6290@gmail.com>

--- expert_system/RhsValidator.py
+++ expert_system/RhsValidator.py
@@ -8,16 +8,36 @@ class RhsValidator(Transformer):
        |,!,^ are forbidden in rhs
     """
     def expr_and(self, child):
+        """expr_and -> true if both
+           children are valid expressions
+
+           expr_and in rhs are allowed
+        """
         return child[0] and child[1]
 
     def expr_or(self, child):
+        """expr_or -> always false
+           not supported as rhs operation
+           in this expert system
+        """
         return False
 
     def expr_xor(self, child):
+        """expr_xor -> always false
+           not supported as rhs operation
+           in this expert system
+        """
         return False
 
     def expr_neg(self, child):
+        """expr_neg -> always false
+           not supported as rhs operation
+           in this expert system
+        """
         return False
 
     def val(self, child):
+        """val -> always true
+           values in rhs are allowed
+        """
         return True
