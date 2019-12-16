f9429f4a1263bb30f598f47fa7f6b017a2d3cb5d
oyevtushe <casio6290@gmail.com>
2019-12-17 01:01:13 +0200

    Improve doc in ResolveRhs
    Signed-off-by: oyevtushe <casio6290@gmail.com>

--- expert_system/ResolveLhs.py
+++ expert_system/ResolveLhs.py
@@ -6,26 +6,41 @@ class ResolveLhs(Transformer):
        dfs
     """
     def transform(self, tree, facts):
+        """transform(self, tree, facts)
+           'tree' - tree to transform
+           'facts' - dict to get info about
+           facts from
+        """
         self._facts = facts
         return Transformer.transform(self, tree)
 
     def expr_and(self, child):
+        """expr_and -> child0 & child1
+        """
         a = child[0]
         b = child[1]
         return a and b
 
     def expr_or(self, child):
+        """expr_or -> child0 | child1
+        """
         a = child[0]
         b = child[1]
         return a or b
 
     def expr_xor(self, child):
+        """expr_xor -> child0 ^ child1
+        """
         a = child[0]
         b = child[1]
         return a ^ b
 
     def expr_neg(self, child):
+        """expr_neg -> !child
+        """
         return not child[1]
 
     def val(self, child):
+        """val -> value of a fact
+        """
         return self._facts[child[0]]
